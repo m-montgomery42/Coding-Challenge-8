ec6e0fb703474197bbd6a9690897237fba254f93
Markayla Montgomery <mm42@usf.edu>
2025-02-16 18:58:24 -0500

    Task 3 - Service Fee Calculation

--- cc_8.js
+++ cc_8.js
@@ -24,3 +24,21 @@ const calculateDiscount = function(price, discountRate) {
   // Final product price
   calculateDiscount(100, 0.2); 
   calculateDiscount(250, 0.15);
+
+  // Task 3: Arrow Function
+// Arrow function to calculate the service fee
+const calculateServiceFee = (amount, serviceType) => {
+    let fee;
+    if (serviceType === "Premium") {
+      fee = amount * 0.15;
+    } else if (serviceType === "Standard") {
+      fee = amount * 0.10;
+    } else if (serviceType === "Basic") {
+      fee = amount * 0.05;
+    }
+    
+    // Output the result
+    console.log(`Service Fee: $${fee.toFixed(2)}`);
+  };
+  calculateServiceFee(150, "Premium");
+  calculateServiceFee(725, "Standard");
